2f32ae441c8f1a881f4de3cf746b203f1af3d7e0
Scott <scott@paragonie.com>
2017-03-27 17:53:54 -0400

    Add base64 to checks
    http://www.cryptofails.com/post/87697461507/46esab-high-quality-cryptography-direct-from

--- fca.sh
+++ fca.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rg -i -e md2 -e md4 -e md5 -e sha1 -e rc2 -e rc4 -e mersenne -e ecb -e 3des -e getpid -e srand $1
+rg -i -e md2 -e md4 -e md5 -e sha1 -e rc2 -e rc4 -e mersenne -e ecb -e 3des -e getpid -e srand -e base64 $1
