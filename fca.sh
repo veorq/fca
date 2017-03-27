a10816d98a02756216ff4217807ed4e11b574d6e
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2017-03-27 22:51:25 +0200

    optimized

--- fca.sh
+++ fca.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-GREP=rg
-WEAK="-e md2 -e md4 -e md5 -e sha1 -e rc2 -e rc4 -e mersenne -e ecb -e 3des -e getpid -e srand"
-$GREP -i $WEAK $1 
-
+rg -i -e md2 -e md4 -e md5 -e sha1 -e rc2 -e rc4 -e mersenne -e ecb -e 3des -e getpid -e srand $1
